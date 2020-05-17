213692d1e9f89f67d3f151604524cbeed7a01bf5
jose.a.aravequia <jose.a.aravequia@noaa.gov>
2020-05-16 20:01:16 -0500

    Add Orion port

--- sorc/build_ncep_post.sh
+++ sorc/build_ncep_post.sh
@@ -1,5 +1,4 @@
-SHELL=/bin/sh
-
+#!/bin/bash
 ####################################################################################################
 #
 # post using module compile standard
@@ -8,42 +7,97 @@ SHELL=/bin/sh
 # 01/16 Lin Gan:	Update to use GFS Vertical Structure
 # 07/16 J. Carley:      Generalize for other machines using modules
 # 07/18 Wen Meng:       Set post to v8.0.0 for fv3gfs
+# 10/19 M Kavulich:     Provide machine name as an input argument
 #
 #####################################################################################################
 #####################################################################################################
 
+#List of valid machines:
+validmachines=(theia jet wcoss_dell_p3 wcoss cray-intel hera orion)
+
+function usage {
+   echo "Usage:"
+   echo " $0 machinename"
+   echo ""
+   echo " Valid values for 'machinename' are: ${validmachines[@]}"
+   exit 1
+}
+
+if [ "$#" -eq 0 ]; then
+   #Check to see if we are building the old way
+   module purge
+   set -x
+   mac=$(hostname | cut -c1-1)
+   mac2=$(hostname | cut -c1-2)
+   if [ $mac2 = tf ] ; then                         # For Theia
+      machine=theia
+   elif [ $mac = f  ] ; then                        # For Jet 
+      machine=jet
+   elif [ $mac = v -o $mac = m  ] ; then            # For Dell
+      machine=wcoss_dell_p3
+   elif [ $mac = t -o $mac = e -o $mac = g ] ; then # For WCOSS
+      machine=wcoss
+   elif [ $mac = l -o $mac = s ] ; then             #    wcoss_c (i.e. luna and surge)
+      export machine=cray-intel
+   elif [ $mac2 = hf ] ; then                       # For Hera
+      machine=hera
+   elif [ $mac2 = Or ] ; then                       # For Orion
+      machine=orion
+   else
+      echo ""
+      echo "ERROR ERROR ERROR"
+      echo ""
+      echo "Error: To use this build script without arguments you must be on a valid machine"
+      echo "Valid machines are:"
+      echo "${validmachines[@]}"
+      echo ""
+      echo "ERROR ERROR ERROR"
+   fi
+
+elif [ "$#" -gt 1 ]; then
+   echo "Error: too many input arguments"
+   exit 2
+else
+   machine=$1
+fi
 
 # Lin Gan Module Load
-module purge
 set -x
-mac=$(hostname | cut -c1-1)
-mac2=$(hostname | cut -c1-2)
-if [ $mac2 = tf ] ; then                        # For Theia
- machine=theia
- . /etc/profile
- . /etc/profile.d/modules.sh
-elif [ $mac = f  ] ; then            # For Jet 
- machine=jet
- . /etc/profile
- . /etc/profile.d/modules.sh
-elif [ $mac = v -o $mac = m  ] ; then            # For Dell
- machine=wcoss_dell_p3
- . $MODULESHOME/init/bash                 
-elif [ $mac = t -o $mac = e -o $mac = g ] ; then # For WCOSS
- machine=wcoss
- . /usrx/local/Modules/default/init/bash
-elif [ $mac = l -o $mac = s ] ; then             #    wcoss_c (i.e. luna and surge)
- export machine=cray-intel
-elif [ $mac2 = hf ] ; then                        # For Hera
- machine=hera
- . /etc/profile
- . /etc/profile.d/modules.sh
-elif [ $mac2 = Or ] ; then                        # For Orion
- machine=orion
- . /etc/profile
-## . /etc/profile.d/modules.sh
+case $machine in
+theia)                                 # For Theia
  module purge
-fi
+ . /etc/profile
+ . /etc/profile.d/modules.sh
+ ;;
+jet)                                   # For Jet
+ module purge
+ . /etc/profile
+ . /etc/profile.d/modules.sh
+ ;;
+wcoss_dell_p3)                         # For Dell
+ module purge
+ . $MODULESHOME/init/bash                 
+ ;;
+wcoss)                                 # For WCOSS
+ module purge
+ . /usrx/local/Modules/default/init/bash
+ ;;
+cray-intel)                            # For wcoss_c (i.e. luna and surge)
+ module purge
+ ;;
+hera)                                  # For Hera
+ . /etc/profile
+ . /etc/profile.d/modules.sh
+ ;;
+orion)                                  # For Orion
+ . /etc/profile
+ ;;
+*)
+ set +x
+ echo "ERROR: Invalid machine name specified"
+ usage
+ ;;
+esac
 
 # Lin Gan modifiy to use NCO vertical structure prefix for NCO deployment - 20160131
 moduledir=`dirname $(readlink -f ../modulefiles/post)`
@@ -60,3 +114,5 @@ if [ ! -d "../../exec" ] ; then
   mkdir -p ../../exec
 fi
 cp ncep_post ../../exec/
+
+exit 0
